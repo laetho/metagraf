2b17c2cc04e40d0f397f854bf9c5884747d65f76
a01595 <lars-erik.thorsplass@norsk-tipping.no>
2018-03-15 14:57:14 +0100

    main() no looks for POC files, loops over them and parse them

--- src/mgraf.go
+++ src/mgraf.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"metagraf/src/metagraf"
+	"path/filepath"
+	"strings"
+)
+
+
 
 func main() {
-	fmt.Println("mgraf")
+	var files []string
+	basepath := "/home/a01595/go/src/metagraf/collections/poc"
+
+	// Wal the directory provided in basepath
+	err := filepath.Walk(basepath, func( path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Loop through all files in the directory given in basepath,
+	// ignore basepath itself, and ignore file names not containing "json"
+	// Parse each json file
+	for _, file := range files {
+		if file == basepath { continue }
+		if !strings.Contains( file, "json") { continue }
+		fmt.Println("Parsing MetaGraf ", file)
+		metagraf.Parse( file )
+	}
 }
