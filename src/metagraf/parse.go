38bdfae5a6173ea17bb18fc4892c66526cdf2f87
a01595 <lars-erik.thorsplass@norsk-tipping.no>
2018-03-15 14:57:42 +0100

    Parse() now reads and unmarshall's json data

--- src/metagraf/parse.go
+++ src/metagraf/parse.go
@@ -1,6 +1,25 @@
 package metagraf
 
-func Parse( filepath string ) {
+import (
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+)
 
+func Parse( filepath string ) {
+	b, err := ioutil.ReadFile( filepath )
+	if err != nil {
+		panic(err)
+	}
+
+	var mg MetaGraf
+
+	json.Unmarshal(b, &mg)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf( "%T", mg )
+	fmt.Println( mg )
 
 }
