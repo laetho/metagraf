8e0ab7414ded8057668ab9a072e3917f66f8f8c0
laetho <thorsplass@gmail.com>
2018-08-28 12:24:20 +0200

    First implementation of service object generation

--- pkg/generators/service.go
+++ pkg/generators/service.go
@@ -19,6 +19,10 @@ package generators
 import (
 	"fmt"
 	"github.com/blang/semver"
+	"github.com/fsouza/go-dockerclient"
+	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"metagraf/pkg/helpers"
 	"metagraf/pkg/metagraf"
 	"strconv"
 	"strings"
@@ -31,12 +35,50 @@ func GenService(mg *metagraf.MetaGraf) {
 	var objname string
 	var serviceports []corev1.ServicePort
 
-
 	sv, err := semver.Parse(mg.Spec.Version)
 	if err != nil {
 		objname = strings.ToLower(mg.Metadata.Name)
 	} else {
 		objname = strings.ToLower(mg.Metadata.Name + "v" + strconv.FormatUint(sv.Major, 10))
+	}
+
+	// todo dockerimage should inspected once and return a pointer to the first instance inspection data
+	var DockerImage string
+	if len(mg.Spec.BaseRunImage) > 0 {
+		DockerImage = mg.Spec.BaseRunImage
+	} else if len(mg.Spec.BuildImage) > 0 {
+		DockerImage = mg.Spec.BuildImage
+	} else {
+		DockerImage = ""
+	}
+
+	auth := docker.AuthConfiguration{
+		Username: viper.GetString("user"),
+		Password: viper.GetString("password"),
+	}
+
+	ImageInfo, err := helpers.DockerInspectImage(DockerImage, "latest", auth)
+	if err != nil {
+		// @todo should we fail here?
+		// Create an empty Image struct so the following logic can run
+		ImageInfo = &docker.Image{
+			Config: &docker.Config{},
+		}
+	}
+
+	for k := range ImageInfo.Config.ExposedPorts {
+		port, _ := strconv.Atoi(k.Port())
+		ContainerPort := corev1.ServicePort{
+			Name: strings.ToUpper(k.Proto())+"-"+k.Port(),
+			Port: int32(port),
+			Protocol:      corev1.Protocol(strings.ToUpper(k.Proto())),
+			TargetPort: intstr.IntOrString{
+				Type: 0,
+				IntVal: int32(port),
+				StrVal: k.Port(),
+			},
+		}
+		serviceports = append(serviceports, ContainerPort)
 	}
 
 	selectors := make(map[string]string)
@@ -57,6 +99,8 @@ func GenService(mg *metagraf.MetaGraf) {
 		Spec: corev1.ServiceSpec{
 			Ports: serviceports,
 			Selector: selectors,
+			Type: "ClusterIP",
+			SessionAffinity: "None",
 		},
 	}
 
