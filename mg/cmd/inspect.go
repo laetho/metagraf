8126eb60dd05bc586df0b151723d08fff3eca7b9
laetho <thorsplass@gmail.com>
2018-09-12 11:48:02 +0200

    Initial cmd stub for inspect functionality

---
+++ mg/cmd/inspect.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var InspectCmd = &cobra.Command{
+	Use:   "inspect <metaGraf>",
+	Short: "inspect a metaGraf specification",
+	Long:  `inspect a metaGraf specification and list objects that will be created or patched.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Active project is:", viper.Get("namespace"))
+			fmt.Println("Missing path to metaGraf specification")
+			return
+		}
+
+		if len(Namespace) == 0 {
+			Namespace = viper.GetString("namespace")
+			if len(Namespace) == 0 {
+				fmt.Println("Namespace must be supplied")
+				os.Exit(1)
+			}
+		}
+
+		//mg := metagraf.Parse(args[0])
+
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(InspectCmd)
+}
