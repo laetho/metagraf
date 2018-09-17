66c3b08ba02361215def1b981541a8e78cd3eeb7
laetho <thorsplass@gmail.com>
2018-09-17 15:17:22 +0200

    Implemented secretExsists() function to figure out if a secret is already defined in a namespace

--- pkg/modules/secret.go
+++ pkg/modules/secret.go
@@ -17,29 +17,71 @@ limitations under the License.
 package modules
 
 import (
-	"fmt"
 	"encoding/json"
-	corev1 "k8s.io/api/core/v1"
+	"fmt"
 	"metagraf/pkg/metagraf"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"metagraf/mg/ocpclient"
 )
 
 func GenSecrets(mg *metagraf.MetaGraf) {
 	for _,r := range mg.Spec.Resources {
+		// Is secret generation necessary
 		if len(r.SecretRef) == 0 && len(r.User) == 0 {
 			continue
 		}
-		obj := genResourceSecret(r)
+
+		// Do not create secret if it already exist
+		if secretExists(ResourceSecretName(&r)) {
+			continue
+		}
+
+		obj := genResourceSecret(&r, mg)
 		ba, err := json.Marshal(obj)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Println(string(ba))
-
 	}
 }
 
-func genResourceSecret(res metagraf.Resource) *corev1.Secret {
-	sec := corev1.Secret{}
+// Check if a named secret exsist in the current namespace.
+func secretExists(name string) bool {
+	cli := ocpclient.GetCoreClient()
+	l, err := cli.Secrets(NameSpace).List(metav1.ListOptions{LabelSelector:"name = "+name})
+
+	if err != nil{
+		panic(err)
+	}
+
+	if len(l.Items) > 0 {
+		return true
+	}
+	return false
+}
+
+func genResourceSecret(res *metagraf.Resource, mg *metagraf.MetaGraf) *corev1.Secret {
+
+	objname := Name(mg)
+
+	// Resource labels
+	l := make(map[string]string)
+	l["name"] = ResourceSecretName(res)
+	l["app"] = objname
+
+
+	sec := corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: objname,
+			Labels: l,
+		},
+	}
 
 	return &sec
 }
