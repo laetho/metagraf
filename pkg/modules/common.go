5b8360a24daa4d861c2fa1827f066a82102a9210
laetho <thorsplass@gmail.com>
2018-09-17 15:16:42 +0200

    Added a function to get a name by convention for a metagraf resource secret.

--- pkg/modules/common.go
+++ pkg/modules/common.go
@@ -23,6 +23,11 @@ import (
 	"strings"
 )
 
+var (
+	NameSpace string	// Used to pass namespace from cmd to module to avoid import cycle.
+)
+
+// Returns a name for a resource based on convention as follows.
 func Name(mg *metagraf.MetaGraf) string {
 	var objname string
 	sv, err := semver.Parse(mg.Spec.Version)
@@ -32,4 +37,17 @@ func Name(mg *metagraf.MetaGraf) string {
 		objname = strings.ToLower(mg.Metadata.Name + "v" + strconv.FormatUint(sv.Major, 10))
 	}
 	return objname
+}
+
+// Returns a name for a secret for a resource based on convention as follows.
+func ResourceSecretName(r *metagraf.Resource) string {
+	if len(r.User) > 0 && len(r.SecretRef) == 0 {
+		// Implicit secret name generation
+		return r.Name+"-"+r.User
+	} else if len(r.User) == 0 && len(r.SecretRef) > 0 {
+		// Explicit secret name generation
+		return r.Name+"-"+r.SecretRef
+	} else {
+		return r.Name
+	}
 }
