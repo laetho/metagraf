c7ca2c14a5e02d05d49b02c5bf293f6e57893389
laetho <thorsplass@gmail.com>
2018-09-18 12:38:03 +0200

    Changes to secrets module after secretref rename to secret

--- pkg/modules/common.go
+++ pkg/modules/common.go
@@ -41,12 +41,12 @@ func Name(mg *metagraf.MetaGraf) string {
 
 // Returns a name for a secret for a resource based on convention as follows.
 func ResourceSecretName(r *metagraf.Resource) string {
-	if len(r.User) > 0 && len(r.SecretRef) == 0 {
+	if len(r.User) > 0 && len(r.Secret) == 0 {
 		// Implicit secret name generation
 		return r.Name+"-"+r.User
-	} else if len(r.User) == 0 && len(r.SecretRef) > 0 {
+	} else if len(r.User) == 0 && len(r.Secret) > 0 {
 		// Explicit secret name generation
-		return r.Name+"-"+r.SecretRef
+		return r.Name+"-"+r.Secret
 	} else {
 		return r.Name
 	}
