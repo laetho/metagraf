40f7eb761ded7ccb5fb62ade4df73bb749f023bf
laetho <thorsplass@gmail.com>
2019-06-19 08:56:43 +0200

    Initial commit of genereate subcommand and generate keys subcommand. Generate keys will produce a source typed source=key=value configfile for a software component. This is intended to be used as the configuration input for mg create commands in deployment scenarios.

---
+++ mg/cmd/generate.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2019 The MetaGraph Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"github.com/golang/glog"
+	"github.com/spf13/cobra"
+	"metagraf/pkg/metagraf"
+	"metagraf/pkg/modules"
+	"os"
+)
+
+func init() {
+	RootCmd.AddCommand(generateCmd)
+	generateCmd.AddCommand(generateKeysCmd)
+}
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "generate operations",
+	Long:  Banner + ` create `,
+}
+
+var generateKeysCmd = &cobra.Command{
+	Use:   "keys <metagraf>",
+	Short: "create configuration keys from metaGraf file",
+	Long:  Banner + `generate keys`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			glog.Error(StrMissingMetaGraf)
+			os.Exit(1)
+		}
+
+		FlagPassingHack()
+
+		mg := metagraf.Parse(args[0])
+		if modules.Variables == nil {
+			vars := MergeSourceVars(
+				mg.GetVarsFromSource(),
+				OverrideVars(mg.GetVars(), CmdCVars(CVars).Parse()))
+			modules.Variables = vars
+		}
+		for k,v := range modules.Variables {
+			fmt.Println(k+"="+v)
+		}
+	},
+}
