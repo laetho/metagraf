ff96a28753dab0ead576d09039b5b25fea3b93d1
laetho <thorsplass@gmail.com>
2019-09-22 22:44:37 +0200

    Added namespace as argument to GenDeployment()

--- pkg/modules/deployment.go
+++ pkg/modules/deployment.go
@@ -19,39 +19,272 @@ package modules
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spf13/viper"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"metagraf/mg/ocpclient"
+	"metagraf/pkg/helpers"
+	"metagraf/pkg/imageurl"
 	"metagraf/pkg/metagraf"
+	"strconv"
+	"strings"
 
 	//corev1 "k8s.io/api/core/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func GenDeployment(mg *metagraf.MetaGraf) {
-
+func GenDeployment(mg *metagraf.MetaGraf, namespace string) {
 	objname := Name(mg)
+	registry := viper.GetString("registry")
+
+	if len(ImageNS) > 0 {
+		namespace = ImageNS
+	}
+
+	// If container registry host is set and it differs from default, use value from -r (--registry) flag.
+	if len(Registry) > 0 && registry != Registry {
+		registry = Registry
+	}
+
 	// Resource labels
 	l := make(map[string]string)
 	l["app"] = objname
+	l["deploymentconfig"] = objname
 
-	obj := appsv1.Deployment{
+	// Selector
+	s := make(map[string]string)
+	s["app"] = objname
+	s["deploymentconfig"] = objname
+
+	var RevisionHistoryLimit int32 = 5
+	var ActiveDeadlineSeconds int64 = 21600
+	var TimeoutSeconds int64 = 600
+	var UpdatePeriodSeconds int64 = 1
+	var IntervalSeconds int64 = 1
+
+	var MaxSurge intstr.IntOrString
+	MaxSurge.StrVal = "25%"
+	MaxSurge.Type = 1
+	var MaxUnavailable intstr.IntOrString
+	MaxUnavailable.StrVal = "25%"
+	MaxUnavailable.Type = 1
+
+	// Instance of RollingDeploymentStrategyParams
+	rollingParams := appsv1.RollingDeploymentStrategyParams{
+		MaxSurge:            &MaxSurge,
+		MaxUnavailable:      &MaxUnavailable,
+		TimeoutSeconds:      &TimeoutSeconds,
+		IntervalSeconds:     &IntervalSeconds,
+		UpdatePeriodSeconds: &UpdatePeriodSeconds,
+	}
+
+	// Containers
+	var Containers []corev1.Container
+	var ContainerPorts []corev1.ContainerPort
+	//var ContainerVolumes []string
+	var Volumes []corev1.Volume
+	var VolumeMounts []corev1.VolumeMount
+	// Environment
+	var EnvVars []corev1.EnvVar
+
+	var DockerImage string
+	if len(mg.Spec.BaseRunImage) > 0 {
+		DockerImage = mg.Spec.BaseRunImage
+	} else if len(mg.Spec.BuildImage) > 0 {
+		DockerImage = mg.Spec.BuildImage
+	} else {
+		DockerImage = ""
+	}
+
+	var imgurl imageurl.ImageURL
+	imgurl.Parse(DockerImage)
+
+	client := ocpclient.GetImageClient()
+
+	ist := helpers.GetImageStreamTags(
+		client,
+		imgurl.Namespace,
+		imgurl.Image+":"+imgurl.Tag)
+
+	ImageInfo := helpers.GetDockerImageFromIST(ist)
+
+	// Adding name and version of component as en environment variable
+	EnvVars = append(EnvVars, corev1.EnvVar{
+		Name:  "MG_APP_NAME",
+		Value: MGAppName(mg),
+	})
+
+	// todo: can be removed?
+	var oversion string
+	if len(Version) > 0 {
+		oversion = Version
+	} else {
+		oversion = mg.Spec.Version
+	}
+
+	EnvVars = append(EnvVars, corev1.EnvVar{
+		Name:  "MG_APP_VERSION",
+		Value: oversion,
+	})
+
+	// Environment Variables from baserunimage
+	if BaseEnvs {
+		for _, e := range ImageInfo.Config.Env {
+			es := strings.Split(e, "=")
+			if helpers.SliceInString(EnvBlacklistFilter, strings.ToLower(es[0])) {
+				continue
+			}
+			EnvVars = append(EnvVars, corev1.EnvVar{Name: es[0], Value: es[1]})
+		}
+	}
+
+	// Handle EnvFrom
+	var EnvFrom []corev1.EnvFromSource
+
+	// Local variables from metagraf as deployment envvars
+	for _, e := range mg.Spec.Environment.Local {
+		// Skip environment variable if SecretFrom
+		if len(e.SecretFrom) > 0 {
+			continue
+		}
+		if len(e.EnvFrom) > 0 {
+			continue
+		}
+		// Use EnvToEnvVar to potentially use override values.
+		EnvVars = append(EnvVars, EnvToEnvVar(&e, false))
+	}
+
+	// External variables from metagraf as deployment envvars
+	for _, e := range mg.Spec.Environment.External.Consumes {
+		EnvVars = append(EnvVars, EnvToEnvVar(&e, true))
+	}
+	for _, e := range mg.Spec.Environment.External.Introduces {
+		EnvVars = append(EnvVars, EnvToEnvVar(&e, true))
+	}
+
+	// EnvVars from ConfigMaps, fetch Metagraf config resources that is of
+	for _, e := range mg.Spec.Environment.Local {
+		if len(e.EnvFrom) == 0 {
+			continue
+		}
+
+		EnvFrom = append(EnvFrom, corev1.EnvFromSource{
+			ConfigMapRef: &corev1.ConfigMapEnvSource{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: e.EnvFrom,
+				},
+			},
+		})
+	}
+
+	/*
+		EnvVars from Secrets. Find all environment variables
+		that containers the SecretFrom field and append to the
+		EnvFrom as EnvFromSource->SecretRef.
+	*/
+	for _, e := range mg.Spec.Environment.Local {
+		if len(e.SecretFrom) == 0 {
+			continue
+		}
+		cmref := corev1.EnvFromSource{
+			SecretRef: &corev1.SecretEnvSource{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: e.SecretFrom,
+				},
+			},
+		}
+		EnvFrom = append(EnvFrom, cmref)
+	}
+
+	/* Norsk Tipping Specific Logic regarding
+	   WLP / OpenLiberty Features. Should maybe
+	   look at some plugin approach to this later.
+	   todo: Add annotations from metagraf to deployment and expose them to pod using downward api.
+	   info: https://kubernetes.io/docs/tasks/inject-data-application/downward-api-volume-expose-pod-information/#the-downward-api
+	*/
+	if len(mg.Metadata.Annotations["norsk-tipping.no/libertyfeatures"]) > 0 {
+		EnvVars = append(EnvVars, corev1.EnvVar{
+			Name:  "LIBERTY_FEATURES",
+			Value: mg.Metadata.Annotations["norsk-tipping.no/libertyfeatures"],
+		})
+	}
+
+	// Labels from baserunimage
+	/*
+		for k, v := range ImageInfo.Config.Labels {
+			if helpers.SliceInString(LabelBlacklistFilter, strings.ToLower(k)) {
+				continue
+			}
+			l[k] = helpers.LabelString(v)
+		}
+	*/
+
+	// ContainerPorts
+	for k := range ImageInfo.Config.ExposedPorts {
+		ss := strings.Split(k, "/")
+		port, _ := strconv.Atoi(ss[0])
+		ContainerPort := corev1.ContainerPort{
+			ContainerPort: int32(port),
+			Protocol:      corev1.Protocol(strings.ToUpper(ss[1])),
+		}
+		ContainerPorts = append(ContainerPorts, ContainerPort)
+	}
+
+	Volumes, VolumeMounts = volumes(mg, ImageInfo)
+
+	// Tying Container PodSpec together
+	Container := corev1.Container{
+		Name:            objname,
+		Image:           registry + "/" + namespace + "/" + SpecName(mg) + ":" + Tag,
+		ImagePullPolicy: corev1.PullAlways,
+		Ports:           ContainerPorts,
+		VolumeMounts:    VolumeMounts,
+		Env:             EnvVars,
+		EnvFrom:         EnvFrom,
+	}
+	Containers = append(Containers, Container)
+
+	// Tying the DeploymentObject together, literally :)
+	obj := appsv1.DeploymentConfig{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "Deployment",
-			APIVersion: "apps/v1",
+			Kind:       "DeploymentConfig",
+			APIVersion: "v1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: objname,
+			Name:   objname,
+			Labels: l,
 		},
-		Spec:   appsv1.DeploymentSpec{},
-		Status: appsv1.DeploymentStatus{},
+		Spec: appsv1.DeploymentConfigSpec{
+			Replicas:             1,
+			RevisionHistoryLimit: &RevisionHistoryLimit,
+			Selector:             s,
+			Strategy: appsv1.DeploymentStrategy{
+				ActiveDeadlineSeconds: &ActiveDeadlineSeconds,
+				Type:                  appsv1.DeploymentStrategyTypeRolling,
+				RollingParams:         &rollingParams,
+			},
+			Template: &corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:   objname,
+					Labels: l,
+				},
+				Spec: corev1.PodSpec{
+					Containers: Containers,
+					Volumes:    Volumes,
+				},
+			},
+		},
+		Status: appsv1.DeploymentConfigStatus{},
 	}
 
-	ba, err := json.Marshal(obj)
-	if err != nil {
-		panic(err)
+	if !Dryrun {
+		StoreDeploymentConfig(obj)
 	}
-	fmt.Println(string(ba))
-
+	if Output {
+		MarshalObject(obj.DeepCopyObject())
+	}
 }
+
 
 /*
 Problaby needs a k8s apps client
