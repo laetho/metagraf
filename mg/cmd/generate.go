92bcafd0b6facd9492a7f7828e365d475dc2724d
laetho <thorsplass@gmail.com>
2019-11-14 08:06:50 +0100

    Added subcommands to generate man pages for the mg utility and markdown

--- mg/cmd/generate.go
+++ mg/cmd/generate.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 	"metagraf/pkg/metagraf"
 	"metagraf/pkg/modules"
 	"os"
@@ -30,6 +31,8 @@ func init() {
 	generateCmd.AddCommand(generatePropertiesCmd)
 	generatePropertiesCmd.Flags().BoolVar(&Defaults, "defaults", false, "Populate Environment variables with default values from metaGraf")
 	generatePropertiesCmd.Flags().StringSliceVar(&CVars, "cvars", []string{}, "Slice of key=value pairs, seperated by ,")
+	generateCmd.AddCommand(generateMarkdownCmd)
+	generateCmd.AddCommand(generateManPagesCmd)
 }
 
 var generateCmd = &cobra.Command{
@@ -38,6 +41,36 @@ var generateCmd = &cobra.Command{
 	Long:  Banner + ` create `,
 }
 
+var generateMarkdownCmd = &cobra.Command{
+	Use:   "markdown",
+	Short: "create markdown documentation for the mg command.",
+	Long:  Banner + `generate markdown`,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := doc.GenMarkdownTree( RootCmd, "./")
+		if err != nil {
+			glog.Fatalf("%v", err)
+			os.Exit(1)
+		}
+	},
+}
+
+var generateManPagesCmd = &cobra.Command{
+	Use:   "manpages",
+	Short: "create manpages for the mg command",
+	Long:  Banner + `generate markdown`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		header := &doc.GenManHeader{
+			Title: "mg",
+			Section: "3",
+		}
+		err := doc.GenManTree( RootCmd, header, "./")
+		if err != nil {
+			glog.Fatalf("%v", err)
+			os.Exit(1)
+		}
+	},
+}
 var generatePropertiesCmd = &cobra.Command{
 	Use:   "properties <metagraf>",
 	Short: "create configuration properties from metaGraf file",
