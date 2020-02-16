c5d4086511680e84d182e4ffa32eaaf6c11f35f7
laetho <thorsplass@gmail.com>
2020-02-16 15:43:00 +0100

    Fixed go vet warnings.

--- pkg/metagraf/types.go
+++ pkg/metagraf/types.go
@@ -26,11 +26,11 @@ type MGVars			map[string]string
 // Structure to hold specification section sourced parameters. Should
 // solve key collisions and generally be a more workable solution.
 type MGProperty struct {
-	Source		string	`json:source`
-	Key			string	`json:key`
-	Value		string	`json:value, omitempty`
-	Required	bool	`json:required, omitempty`
-	Default		string	`json:default, omitempty`
+	Source		string	`json:"source"`
+	Key			string	`json:"key"`
+	Value		string	`json:"value,omitempty"`
+	Required	bool	`json:"required,omitempty"`
+	Default		string	`json:"default,omitempty"`
 }
 
 // map for holding MGProperty structs, should be keyed by
