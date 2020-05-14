c7b14f4b0afd871a1d40583ababb28ff7bdd8955
a01595 <lars-erik.thorsplass@norsk-tipping.no>
2020-05-14 16:07:07 +0200

    Refactored to adhere to params.SourceRef

--- pkg/modules/buildconfig.go
+++ pkg/modules/buildconfig.go
@@ -165,11 +165,12 @@ func genBinaryBuildSource() buildv1.BuildSource {
 
 func genGitBuildSource(mg *metagraf.MetaGraf) buildv1.BuildSource {
 	var branch string
-	if len(Branch) > 0 {
-		branch = Branch
+	if len(params.SourceRef) > 0 {
+		branch = params.SourceRef
 	} else {
 		branch = mg.Spec.Branch
 	}
+
 	return buildv1.BuildSource{
 		Type: "Git",
 		Git: &buildv1.GitBuildSource{
