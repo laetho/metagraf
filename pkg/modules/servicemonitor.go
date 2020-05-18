514ed28440cbe3cd026c44349dcf1019510c8caa
a01595 <lars-erik.thorsplass@norsk-tipping.no>
2020-05-18 10:37:01 +0200

    Made creation of ServiceMonitor resources Service aware, using defaults from params now as well. Also added create and delete operations.

--- pkg/modules/servicemonitor.go
+++ pkg/modules/servicemonitor.go
@@ -2,32 +2,50 @@ package modules
 
 import (
 	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"metagraf/mg/k8sclient"
+	"metagraf/mg/params"
 	"metagraf/pkg/metagraf"
+	"fmt"
+	"os"
+	log "k8s.io/klog"
 )
 
-func GenServiceMonitor(mg *metagraf.MetaGraf) {
+func GenServiceMonitorAndService(mg *metagraf.MetaGraf) {
+
+}
+
+func GenServiceMonitor(mg *metagraf.MetaGraf, svc *corev1.Service) {
 	objname := Name(mg)
 	// Resource labels
 	l := make(map[string]string)
 	l["app"] = objname
 	l["deploymentconfig"] = objname
+	l["prometheus"] = params.ServiceMonitorOperatorName
 
 	// Selector
 	s := make(map[string]string)
 	s["app"] = objname
-	s["deploymentconfig"] = objname
 
-
-	// todo: we need to detect this from service object
-	eps := []monitoringv1.Endpoint{}
-	ep := monitoringv1.Endpoint{
-		Port:                 "8080",
-		Path:                 "/metrics",
-		Scheme:               "http",
-		Interval:             "30s",
+	var metricPort corev1.ServicePort
+	for _,p := range svc.Spec.Ports {
+		if p.Port == params.ServiceMonitorPort {
+			metricPort = p
+		}
+	}
+	if metricPort.Port != params.ServiceMonitorPort {
+		fmt.Printf("ERROR: Unable to find default or provided service port for scraping. Tried to find: %v", params.ServiceMonitorPort)
+		os.Exit(1)
 	}
 
+	eps := []monitoringv1.Endpoint{}
+	ep := monitoringv1.Endpoint{
+		Port:                 metricPort.Name,
+		Path:                 params.ServiceMonitorPath,
+		Scheme:               params.ServiceMonitorScheme,
+		Interval:             params.ServiceMonitorInterval,
+	}
 	eps = append(eps, ep)
 
 	var obj = monitoringv1.ServiceMonitor{
@@ -58,18 +76,18 @@ func GenServiceMonitor(mg *metagraf.MetaGraf) {
 }
 
 func StoreServiceMonitor(obj monitoringv1.ServiceMonitor) {
-/*	client := ocpclient.GetBuildClient().BuildConfigs(NameSpace)
-	bc, _ := client.Get(obj.Name, metav1.GetOptions{})
+	client := k8sclient.GetMonitoringV1Client().ServiceMonitors(NameSpace)
+	res, _ := client.Get(obj.Name, metav1.GetOptions{})
 
-	if len(bc.ResourceVersion) > 0 {
-		obj.ResourceVersion = bc.ResourceVersion
+	if len(res.ResourceVersion) > 0 {
+		obj.ResourceVersion = res.ResourceVersion
 		_, err := client.Update(&obj)
 		if err != nil {
 			log.Error(err)
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println("Updated BuildConfig: ", obj.Name, " in Namespace: ", NameSpace)
+		fmt.Println("Updated ServiceMonitor: ", obj.Name, " in Namespace: ", NameSpace)
 	} else {
 		_, err := client.Create(&obj)
 		if err != nil {
@@ -77,27 +95,24 @@ func StoreServiceMonitor(obj monitoringv1.ServiceMonitor) {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println("Created BuildConfig: ", obj.Name, " in Namespace: ", NameSpace)
+		fmt.Println("Created ServiceMonitor: ", obj.Name, " in Namespace: ", NameSpace)
 	}
- */
 }
 
 func DeleteServiceMonitor(name string) {
-	/*
-	client := ocpclient.GetBuildClient().BuildConfigs(NameSpace)
+	client := k8sclient.GetMonitoringV1Client().ServiceMonitors(NameSpace)
 
 	_, err := client.Get(name, metav1.GetOptions{})
 	if err != nil {
-		fmt.Println("The BuildConfig: ", name, "does not exist in namespace: ", NameSpace,", skipping...")
+		fmt.Println("The ServiceMonitor: ", name, "does not exist in namespace: ", NameSpace,", skipping...")
 		return
 	}
 
 	err = client.Delete(name, &metav1.DeleteOptions{})
 	if err != nil {
-		fmt.Println( "Unable to delete BuildConfig: ", name, " in namespace: ", NameSpace)
+		fmt.Println( "Unable to delete ServiceMonitor: ", name, " in namespace: ", NameSpace)
 		log.Error(err)
 		return
 	}
-	fmt.Println("Deleted BuildConfig: ", name, ", in namespace: ", NameSpace)
-	 */
+	fmt.Println("Deleted ServiceMonitor: ", name, ", in namespace: ", NameSpace)
 }
