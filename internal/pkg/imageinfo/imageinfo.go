6febc250ae3e18d9131575f94acb17a10f9b9f9c
laetho <thorsplass@gmail.com>
2021-02-14 12:19:28 +0100

    Introduced MGImageInfo()

--- internal/pkg/imageinfo/imageinfo.go
+++ internal/pkg/imageinfo/imageinfo.go
@@ -5,8 +5,28 @@ import (
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"metagraf/pkg/metagraf"
 )
 
+
+// Returns the oci or docker image config section based on images referenced
+// in the metaGraf specification.
+func MGImageInfo(mg *metagraf.MetaGraf) (v1.Config, error) {
+	var imageref string
+	if len(mg.Spec.BaseRunImage) > 0 {
+		imageref = mg.Spec.BaseRunImage
+	} else if len(mg.Spec.BuildImage) > 0 {
+		imageref = mg.Spec.BuildImage
+	} else if len(mg.Spec.Image) > 0 {
+		imageref = mg.Spec.Image
+	}
+
+	config, err := ImageInfo(imageref)
+	if err != nil {
+		return v1.Config{}, err
+	}
+	return config, nil
+}
 
 
 func ImageInfo(imageref string) (v1.Config, error) {
