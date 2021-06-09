41073df804b75af39da8301e2ab2de23141218fb
laetho <thorsplass@gmail.com>
2021-06-09 11:55:17 +0200

    WIP Implementation of mg dev watch - a skaffold like experience for metagraf.

--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 	github.com/coreos/prometheus-operator v0.41.1
 	github.com/crossplane/crossplane-runtime v0.9.0 // indirect
 	github.com/crossplane/oam-kubernetes-runtime v0.0.9
+	github.com/fsnotify/fsnotify v1.4.9
 	github.com/ghodss/yaml v1.0.0
+	github.com/go-fsnotify/fsnotify v0.0.0-20180321022601-755488143dae // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/google/go-containerregistry v0.4.0
 	github.com/google/gofuzz v1.2.0 // indirect
--- mg/cmd/dev.go
+++ mg/cmd/dev.go
@@ -21,7 +21,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"sync"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/laetho/metagraf/internal/pkg/params"
 	"github.com/laetho/metagraf/pkg/metagraf"
 	"github.com/laetho/metagraf/pkg/modules"
@@ -29,6 +33,17 @@ import (
 	"github.com/spf13/viper"
 	corev1 "k8s.io/api/core/v1"
 	log "k8s.io/klog"
+)
+
+var wg sync.WaitGroup
+
+// Type for events emitted by filteredFileWatcher.
+type WatchEvent int
+
+// Enum values for WatchEvent
+const (
+	BuildAndDeploy WatchEvent = iota
+	Deploy
 )
 
 func init() {
@@ -51,7 +66,6 @@ func init() {
 	devCmdUp.Flags().Int32Var(&params.ServiceMonitorPort, "service-monitor-port", params.ServiceMonitorPort, "Set Service port to scrape in ServiceMonitor.")
 	devCmdUp.Flags().StringVar(&params.ServiceMonitorOperatorName, "service-monitor-operator-name", params.ServiceMonitorOperatorName, "Name of prometheus-operator instance to create ServiceMonitor for.")
 
-
 	devCmd.AddCommand(devCmdDown)
 	devCmdDown.Flags().StringVarP(&params.NameSpace, "namespace", "n", "", "namespace to work on, if not supplied it will use current active namespace.")
 	devCmdDown.Flags().BoolVar(&params.Everything, "everything", false, "Delete all resources and artifacts generated from mg dev up.")
@@ -61,12 +75,62 @@ func init() {
 	devCmdBuild.Flags().StringVar(&params.SourceRef, "ref", "master", "Specify the git ref or branch ref to build.")
 	devCmdBuild.Flags().StringVarP(&params.NameSpace, "namespace", "n", "", "namespace to work on, if not supplied it will use current active namespace.")
 	devCmdBuild.Flags().BoolVar(&params.LocalBuild, "local", false, "Builds application from src in current (.) direcotry.")
+
+	devCmd.AddCommand(devCmdWatch)
+	devCmdWatch.Flags().StringVarP(&params.NameSpace, "namespace", "n", "", "namespace to work on, if not supplied it will use current active namespace.")
+	devCmdWatch.Flags().StringSliceVar(&CVars, "cvars", []string{}, "Slice of key=value pairs, seperated by ,")
+	devCmdWatch.Flags().StringVar(&params.PropertiesFile, "cvfile", "", "Property file with component configuration values. Can be generated with \"mg generate properties\" command.)")
 }
 
 var devCmd = &cobra.Command{
 	Use:   "dev",
 	Short: "dev subcommands",
 	Long:  `dev subcommands`,
+}
+
+var devCmdWatch = &cobra.Command{
+	Use:   "watch <metagraf.json>",
+	Short: "watches for local filechanges and rebuilds or redeploys the component.",
+	Long:  `Inspired by skaffold.dev. Watches for local file changes and rebuilds and redeploys.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		requireMetagraf(args)
+		requireNamespace()
+
+		mg := metagraf.Parse(args[0])
+		bc := mg.Name(OName, Version)
+
+		FlagPassingHack()
+		modules.NameSpace = params.NameSpace
+
+		// Crate a buffered channel with room for one event.
+		chEvents := make(chan WatchEvent, 1)
+		// Channel to indicate if we are in processing state.
+		chProcessing := make(chan bool, 1)
+
+		wg.Add(2)
+
+		go filteredFileWatcher(chEvents, chProcessing)
+
+		for {
+			select {
+			case command := <-chEvents:
+				chProcessing<-true
+				switch command {
+				case BuildAndDeploy:
+					buildGenerate(&mg, params.NameSpace, true)
+					err := s2ibuild(bc, params.NameSpace, true)
+					if err != nil {
+						log.Fatalf("Unable to build: %v ", err)
+					}
+					devUp(args[0])
+				case Deploy:
+					devUp(args[0])
+				}
+				chProcessing<-false
+			}
+		}
+		wg.Wait()
+	},
 }
 
 var devCmdUp = &cobra.Command{
@@ -113,36 +177,12 @@ var devCmdBuild = &cobra.Command{
 		FlagPassingHack()
 		modules.NameSpace = params.NameSpace
 
-		// Remove RepSecRef from generated BuildConfig if --local argument is provided.
-		if params.LocalBuild && len(mg.Spec.RepSecRef) > 0 {
-			mg.Spec.RepSecRef = ""
-		}
-
-		modules.GenImageStream(&mg, params.NameSpace)
-		modules.GenBuildConfig(&mg)
-
-		path, err := exec.LookPath("oc")
+		buildGenerate(&mg, params.NameSpace, params.LocalBuild)
+		err := s2ibuild(bc, params.NameSpace, params.LocalBuild)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("Build failed: %v", err)
 		}
-		arg := []string{"start-build", bc,"-n", params.NameSpace, "--follow"}
-		if params.LocalBuild {
-			arg = append(arg, "--from-file=.")
-		}
-		c := exec.Command(path, arg...)
-		stdout, err := c.StdoutPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-		c.Env = append(os.Environ())
-		c.Start()
 
-		buf := bufio.NewReader(stdout)
-		line := []byte{}
-		for err == nil {
-			line, _,err = buf.ReadLine()
-			fmt.Println(string(line))
-		}
 	},
 }
 
@@ -177,10 +217,111 @@ func devDown(mgf string) {
 	}
 }
 
-func checkForOC() bool {
-	_, err := exec.LookPath("oc")
-	if err != nil {
-		return false
+func buildGenerate(mg *metagraf.MetaGraf, ns string, local bool) {
+	// Remove RepSecRef from generated BuildConfig if --local argument is provided.
+	if local && len(mg.Spec.RepSecRef) > 0 {
+		mg.Spec.RepSecRef = ""
 	}
-	return true
+
+	modules.GenImageStream(mg, ns)
+	modules.GenBuildConfig(mg)
+}
+
+func s2ibuild(bc string, ns string, local bool) error {
+	path, err := exec.LookPath("oc")
+	if err != nil {
+		return err
+	}
+
+	arg := []string{"start-build", bc, "-n", ns, "--follow"}
+	if local {
+		arg = append(arg, "--from-file=.")
+	}
+	c := exec.Command(path, arg...)
+	stdout, pipeerr := c.StdoutPipe()
+	if pipeerr != nil {
+		return pipeerr
+	}
+	c.Env = append(os.Environ())
+	cmderr := c.Start()
+	if cmderr != nil {
+		return cmderr
+	}
+
+	buf := bufio.NewReader(stdout)
+	line := []byte{}
+	for err == nil {
+		line, _, err = buf.ReadLine()
+		fmt.Println(string(line))
+	}
+	return nil
+}
+
+// Watches for events in ./ using fsnotify and writes WatchEvent's to typed channel.
+// Reads processing state from a bool channel. If true, skip writing commands.
+func filteredFileWatcher(chEvents chan<- WatchEvent, chProcessing <-chan bool) {
+
+	done := make(chan bool)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	watchDir := func(path string, info os.FileInfo, err error) error {
+		if info.Mode().IsDir() {
+			return watcher.Add(path)
+		}
+		return nil
+	}
+
+	if err := filepath.Walk("./", watchDir); err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		var skipevents bool = false
+		for {
+			select {
+			case event := <-watcher.Events:
+				// Check to see if we got a new chProcessing state to handle
+				if len(chProcessing) >= 1 {
+					skipevents = <-chProcessing
+				}
+				if skipevents {
+					// Pop of the Event and continue.
+					<-watcher.Events
+					continue
+				}
+
+				// Discard temporary files from editors
+				if strings.Contains(event.Name, "~") {
+					continue
+				}
+				if len(event.Name) == 0 {
+					continue
+				}
+
+				switch event.Op.String() {
+				case "WRITE":
+					log.V(2).Infof("Got event on: %v, Type: %v", event.Name, event.Op)
+					// Redeploy if configuration input changed.
+					if strings.Contains(event.Name, params.PropertiesFile) {
+						log.Info("Properties file changed, redeploying.")
+						chEvents <- Deploy
+					} else if strings.Contains(event.Name, "metagraf.json") {
+						log.Info("metaGraf Specification Changed")
+						chEvents <- BuildAndDeploy
+					}else {
+						// Build and Redeploy
+						chEvents <- BuildAndDeploy
+					}
+				}
+			case err := <-watcher.Errors:
+				fmt.Println("ERROR:", err)
+			default:
+			}
+		}
+	}()
+	<-done
 }
