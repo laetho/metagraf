00bee56051491c3654c5bf81665f9c36cedf9656
Robert Ofstad <Robert.Ofstad@norsk-tipping.no>
2021-10-14 15:14:38 +0200

    Added support to build images with pre built binaries

--- internal/pkg/params/dev.go
+++ internal/pkg/params/dev.go
@@ -1,9 +1,12 @@
 package params
 
 var(
-	// Boolean flag to indicate if we should create empty secrets.
+	// CreateSecrets Boolean flag to indicate if we should create empty secrets.
 	CreateSecrets bool = false
 
-	// Boolean file to indicate we want to build from local directory.
+	// LocalBuild Boolean flag to indicate we want to build from local directory.
 	LocalBuild bool = false
+
+	// FileBuild Give path to a file that should be copied to the container image being built (i.e. my.war).
+	FileBuild string = ""
 )
